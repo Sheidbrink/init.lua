bbd32d0e9427fe3f6abe2f01f9c6cd82443cb32f
Scott Heidbrink <scott.heidbrink@gmail.com>
2023-05-10 21:39:09 -0600

    Update cheatsheet

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 require("scott")
 
+--:message
+--:checkhealth
+
 --<C-^> -- prev file
 --* -- search for word under here
 --[[ 
@@ -8,3 +11,10 @@ require("scott")
     @<register>
 ]]--
 
+--<leader> gs - git window
+--s - stage
+--u - unstage
+--U - unstage all
+--I/P - patch
+--cc - commit
+--Git push - push
