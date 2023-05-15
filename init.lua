fa9e42c8f1ac61df314934742aff2c031ab0fb20
Scott Heidbrink <scott.heidbrink@gmail.com>
2023-05-15 09:09:34 -0600

    Resize cheatsheet

--- init.lua
+++ init.lua
@@ -18,3 +18,10 @@ require("scott")
 --I/P - patch
 --cc - commit
 --Git push - push
+--CTRL-W = - Resize windows for them to fit on the screen with the same size.
+--CTRL-W - - Decrease window’s height.
+--CTRL-W + - Increase window’s height.
+--CTRL-W < - Decrease window’s width.
+--CTRL-W > - Increase window’s width.
+--:help operator
+--:help text-objects
