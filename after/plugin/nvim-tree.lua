b41c0ff061eaa1983a566435968183321b995cb5
Scott Heidbrink <scott.heidbrink@gmail.com>
2023-05-15 23:48:21 -0600

    nvim-tree don't ignore files and don't resize

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,4 +1,14 @@
 require("nvim-web-devicons").setup()
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    actions = {
+        open_file = {
+            resize_window = false,
+            --window_picker = { enable = false}
+        }
+    },
+    git = {
+        ignore = false
+    }
+})
 
 vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
